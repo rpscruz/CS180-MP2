38e8c161bb95d152fc01e1d58b22500d8fdb94e3
RC <rscruz7@up.edu.ph>
2019-05-18 23:22:19 +0800

    using sklearn for neural networks

--- datasets.py
+++ datasets.py
@@ -1,19 +1,13 @@
 import os
 import nibabel
-import matplotlib.pyplot as plt
 import numpy as np
-import skimage.segmentation as seg
-from skimage.exposure import histogram
-import cv2
-from scipy.misc import imsave
+import imageio
 
 dir_path = os.getcwd()
 
-
-from scipy.ndimage import zoom
-def load_scans(samples_len):
-    scans = np.ndarray(shape=(0, 512, 512))
-    masks = np.ndarray(shape=(0, 512, 512))
+def convert_3d_to_2d(samples_len):
+    # scans = np.ndarray(shape=(0, 512, 512))
+    # masks = np.ndarray(shape=(0, 512, 512))
     print(scans.shape)
     print(scans)
 
@@ -22,7 +16,7 @@ def load_scans(samples_len):
         print(str(i).rjust(2, '0'))
         scan_filename = "{}/dataset_preprocess/train/scans/Patient_{}.nii.gz".format(dir_path, str(i).rjust(2, '0'))
         mask_filename = "{}/dataset_preprocess/train/masks/Patient_{}_GT.nii.gz".format(dir_path, str(i).rjust(2, '0'))
-
+        print(mask_filename)
         print("load_scans: loaded file")
         scan = nibabel.load(scan_filename)
         mask = nibabel.load(mask_filename)
@@ -31,34 +25,36 @@ def load_scans(samples_len):
         scan_data = scan.get_data()
         mask_data = mask.get_data()
 
-        print(mask_data)
-       
-        print(mask_data.shape)
         scan_data = scan_data.T
         mask_data = mask_data.T
-        print(mask_data.shape)
-        '''
+        result = np.where(mask_data>0, 255, mask_data) 
+
+        
         for slide in range(len(scan_data)):
-            print(scan_data[slide])
-            imsave("data\\scans\\{}_{}.png".format(i, slide), scan_data[slide])
-            imsave("data\\masks\\{}_{}.png".format(i, slide), mask_data[slide])
-        '''
-        for slide in range(len(mask_data)):
-            print(mask_data[slide])
-            imsave("data\\masks\\{}_{}.png".format(i, slide), mask_data[slide])
-
-
-        # scans = np.concatenate((scans, scan_data), axis=0)
-        '''
-        plt.imshow(scans[0], 'gray')
-        plt.show()
-        '''
+            imageio.imwrite("data/scans/{}_{}.png".format(i, slide), scan_data[slide])
+            imageio.imwrite("data/masks/{}_{}.png".format(i, slide), result[slide])
+        
 
         print(scans.shape)
         print(scan_data.shape)
-
-        # test = concatenate((scan_data, scans), axis=0)
-    return scans, masks
+    return
 
    
-scans, masks = load_scans(3)
+# convert_3d_to_2d(40)
+
+import glob
+
+def load_scans():
+    scans, masks = [], []
+    for scan_path in glob.glob("{}/data/scans/*_*.png".format(dir_path)):
+        scan = imageio.imread(scan_path)
+        scans.append(scan)
+
+    k = 0
+    for mask_path in glob.glob("{}/data/masks/*_*.png".format(dir_path)):
+        mask = imageio.imread(mask_path)
+        masks.append(mask)
+
+    scans = np.asarray(scans)
+    masks = np.asarray(masks)
+    return scans, masks
